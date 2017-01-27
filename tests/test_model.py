519089bfa24ac91f8499d1083648093bf894d577
Kevin Pitolin <kevin@Kevins-MacBook-Pro.local>
2017-01-27 10:53:44 +0100

    Initial commit

---
+++ tests/test_model.py
@@ -0,0 +1,17 @@
+import unittest
+from context import score
+class ModelTestCase(unittest.TestCase):
+	#def setUp(self):
+
+	# #Score tests cases
+	# def test_inverse_document_frequency_zero_containing(self):
+	# 	nb_docs_containing = 0
+	# 	nb_docs = 1
+	# 	self.assertTrue(math.isnan(score.inverse_document_frequency(nb_docs_containing,nb_docs)))
+
+
+
+	#def tearDown(self):
+
+if __name__=='__main__':
+	unittest.main()
